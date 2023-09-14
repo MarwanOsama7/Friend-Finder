8684d3a64e365fe7adf4a72b7da73c1705d242bb
MarwanOsama7 <omarwan773@gmail.com>
2023-09-14 20:18:29 +0300

    first commit

---
+++ src/test/java/com/global/FriendsFinderBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.global;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendsFinderBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
